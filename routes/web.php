979e5fa8109ca12b47a5a6bbeebd43073709c7e7
juan choque <choqueguzmaj@gmail.com>
2025-05-13 03:37:04 -0400

    fourcommit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/landin', function () {
     return view('landin');
 });
+
+// Route::get('/landin', function () {
+//     return view('landin');
+// });
